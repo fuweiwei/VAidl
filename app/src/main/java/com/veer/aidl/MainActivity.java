88d46225137d6fe242e1d213e6f4d1ecd69c05c2
fuweiwei <fuweiwei@kjtpay.com.cn>
2018-08-27 16:09:12 +0800

    显示信息

--- app/src/main/java/com/veer/aidl/MainActivity.java
+++ app/src/main/java/com/veer/aidl/MainActivity.java
@@ -1,15 +1,41 @@
 package com.veer.aidl;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private MyService.MyBinder mMyBinder;
+    private TextView mTvClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startService(new Intent(this, MyService.class));
+        setTitle("服务端");
+        mTvClient = (TextView) findViewById(R.id.tv_client);
+        Intent intent = new Intent(this,MyService.class);
+        bindService(intent, mSCoon, BIND_AUTO_CREATE);
+    }
+
+
+    private ServiceConnection mSCoon = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mMyBinder = (MyService.MyBinder) IMyAidlInterface.Stub.asInterface(service);
+            mMyBinder.setActivity(MainActivity.this);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mSCoon = null;
+        }
+    };
+    public void setText(String s){
+        mTvClient.setText(s);
     }
 }
--- client/src/main/java/com/veer/aidl/client/MainActivity.java
+++ client/src/main/java/com/veer/aidl/client/MainActivity.java
@@ -10,18 +10,22 @@ import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.veer.aidl.IMyAidlInterface;
 
 public class MainActivity extends AppCompatActivity {
 
     IMyAidlInterface iMyAidlInterface;
+    private TextView mTvService;
 
     @SuppressLint("WrongConstant")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("客户端");
+        mTvService = (TextView) findViewById(R.id.tv_service);
         //绑定服务
         Intent intent = new Intent();
         intent.setAction("com.veer.aidl.MyService");
@@ -42,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
         try {
             if(iMyAidlInterface!=null){
                 int res = iMyAidlInterface.add(1, 2);
+                mTvService.setText("从服务端调用成功的结果：" + res);
                 Log.i("client", "从服务端调用成功的结果：" + res);
             }
         } catch (RemoteException e) {
